143134465385ede5a5bef2827b1231a92e211839
Alex Katrinets <itkerry@mail.ru>
2017-06-14 23:05:07 +0300

    Fixed AddPhrase

--- WpfApp1/ViewModel/MainViewModel.cs
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.ObjectModel;
-using System.Runtime.Serialization.Formatters;
 using System.Windows;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
@@ -9,70 +8,45 @@ namespace WpfApp1.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        //private RelayCommand _addPhrase;
-        //public RelayCommand AddPhrase
-        //{
-        //    get { return _addPhrase ?? (_addPhrase = new RelayCommand(() =>
-        //    {
-        //        Items.Add(MyText);
-        //        MyText = null;
-        //    })); }
-        //}
-
-        //public RelayCommand AddPhrase
-        //{
-        //    get
-        //    {
-        //        if (_myText != null)
-        //        {
-        //            return new RelayCommand(() => Items.Add(MyText));
-        //        }
-        //        else
-        //        {
-        //            return new RelayCommand(() => MessageBox.Show("Please input correct value", "Error", MessageBoxButton.OK, MessageBoxImage.Error));
-        //        }
-        //    }
-        //}
-
-        public RelayCommand AddPhrase => new RelayCommand(() =>
-        {
-            Items.Add(MyText);
-            MyText = null;
-        });
+        public RelayCommand AddPhrase => new RelayCommand(() => AddPhraseCommand());
 
         public RelayCommand Clear => new RelayCommand(() => Items.Clear());
 
-        private string gtext;
-
-        public RelayCommand Generate => new RelayCommand(() =>
-        {
-            Random glength = new Random();
-            Random Rnd = new Random();
-            gtext = null;
-            for (int i = 0; i <= glength.Next(0, Items.Count); i++)
-            {
-                if (i == 0)
-                    gtext = Items[Rnd.Next(0, Items.Count)];
-                else 
-                    gtext += ' '+Items[Rnd.Next(0, Items.Count)];
-            }
-            GPhrase = gtext;
-        });
+        public RelayCommand Generate => new RelayCommand(() => GenerateCommand());
 
         public MainViewModel()
         {
             Items = new ObservableCollection<string>();
         }
 
+        private void AddPhraseCommand()
+        {
+            if (!string.IsNullOrEmpty(MyText))
+                Items.Add(MyText);
+            else
+                MessageBox.Show("Please input correct value", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void GenerateCommand()
+        {
+            Random glength = new Random();
+            Random Rnd = new Random();
+            string gtext = null;
+            for (int i = 0; i <= glength.Next(0, Items.Count); i++)
+            {
+                if (i == 0)
+                    gtext = Items[Rnd.Next(0, Items.Count)];
+                else
+                    gtext += ' ' + Items[Rnd.Next(0, Items.Count)];
+            }
+            GPhrase = gtext;
+        }
+
         private string _gprase;
         public string GPhrase
         {
-            get { return _gprase; }
-            set
-            {
-                _gprase = value;
-                RaisePropertyChanged(() => GPhrase);
-            }
+            get => _gprase;
+            set => Set(() => GPhrase, ref _gprase, value);
         }
 
         private string _myText;
