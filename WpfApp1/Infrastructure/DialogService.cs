d5f723576216f715edb3cfee8699276a2029e996
Alex-Mac-WM <oleksandr.katrynets@devrecords.com>
2017-06-20 11:41:49 +0300

    DialogService implementation

--- WpfApp1/Infrastructure/DialogService.cs
+++ WpfApp1/Infrastructure/DialogService.cs
@@ -7,30 +7,35 @@ using MahApps.Metro.Controls.Dialogs;
 
 namespace WpfApp1.Infrastructure
 {
+    //TODO: afterHideCallback not implemented. IMHO MatApps message should be overrided with callback!
     public class DialogService : IDialogService
     {
-        public Task ShowError(string message, string title, string buttonText, Action afterHideCallback)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task ShowError(string message, string title, string buttonText, Action afterHideCallback) =>
+            await ((MetroWindow) Application.Current.MainWindow).ShowMessageAsync(title, message, 
+                MessageDialogStyle.Affirmative,new MetroDialogSettings {AffirmativeButtonText = buttonText});
 
-        public Task ShowError(Exception error, string title, string buttonText, Action afterHideCallback)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task ShowError(Exception error, string title, string buttonText, Action afterHideCallback) =>
+            await((MetroWindow) Application.Current.MainWindow).ShowMessageAsync(title, error.Message,
+                MessageDialogStyle.Affirmative, new MetroDialogSettings {AffirmativeButtonText = buttonText});
 
         public async Task ShowMessage(string message, string title) =>
             await ((MetroWindow)Application.Current.MainWindow).ShowMessageAsync(title, message);
 
-        public Task ShowMessage(string message, string title, string buttonText, Action afterHideCallback)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task ShowMessage(string message, string title, string buttonText, Action afterHideCallback) =>
+            await((MetroWindow) Application.Current.MainWindow).ShowMessageAsync(title, message,
+                MessageDialogStyle.Affirmative, new MetroDialogSettings {AffirmativeButtonText = buttonText});
 
-        public Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText,
+        public async Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText,
             Action<bool> afterHideCallback)
         {
-            throw new NotImplementedException();
+            await ((MetroWindow) Application.Current.MainWindow).ShowMessageAsync(title, message,
+                MessageDialogStyle.AffirmativeAndNegative,
+                new MetroDialogSettings
+                {
+                    AffirmativeButtonText = buttonConfirmText,
+                    NegativeButtonText = buttonCancelText
+                });
+            return false;
         }
 
         public Task ShowMessageBox(string message, string title)
